01dad8c1b9cb840e00f9f28615f4443857086e2c
Choojj <qweasd9526@naver.com>
2020-06-10 22:40:05 +0900

    2020-06-10

--- 단계별/16. 동적 계획법 1/07. 계단 오르기.py
+++ 단계별/16. 동적 계획법 1/07. 계단 오르기.py
@@ -6,9 +6,17 @@ stair_score = []
 for i in range(N):
     stair_score.append(int(sys.stdin.readline()))
 
-stair_sum = [[0, 0] for _ in range(N)]
+stair_sum = [[0] for _ in range(N)]
 
 for i in range(N):
-    
-print(stair_score)
-print(stair_sum)
+    if (i == 0):
+        stair_sum[i] = stair_score[0]
+    elif (i == 1):
+        stair_sum[i] = max(stair_score[0] + stair_score[1], stair_score[1])
+    elif (i == 2):
+        stair_sum[i] = max(stair_score[0] + stair_score[2], stair_score[1] + stair_score[2])
+
+    else:
+        stair_sum[i] = max(stair_sum[i - 3] + stair_score[i - 1] + stair_score[i], stair_sum[i - 2] + stair_score[i])
+
+print(stair_sum[-1])
